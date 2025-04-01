c4b54d7e21e81b784839cfbcc9b5d8ab5bbf393a
minwoo817 <capzhang123@naver.com>
2025-04-01 10:32:21 +0900

    Initial commit

---
+++ src/main/java/example/_엔터티/AppStart.java
@@ -0,0 +1,11 @@
+package example._엔터티;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStart {
+    public static void main(String[] args) {
+        SpringApplication.run( AppStart.class );
+    }
+}
