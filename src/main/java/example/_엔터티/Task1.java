f106b478614b982b3d17cd51fff1212f38120672
minwoo817 <capzhang123@naver.com>
2025-04-01 11:56:10 +0900

    .

---
+++ src/main/java/example/_엔터티/Task1.java
@@ -0,0 +1,26 @@
+package example._엔터티;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tasktodo")
+public class Task1 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private boolean state;
+
+    @Column(nullable = false)
+    private LocalDate createat;
+
+    @Column(nullable = false)
+    private LocalDateTime updateat;
+}
